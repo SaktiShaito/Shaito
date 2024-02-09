6fe35f88a67f7392ef9132ee23f3e9ead6c2c491
SaktiShaito <159524786+SaktiShaito@users.noreply.github.com>
2024-02-09 14:20:40 +0530

    Create Hello.sol

---
+++ Hello.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract hello{
+    string hey="Hello World";
+    int i=10;
+    uint j=120;
+    function display (int i) public view returns(int){
+        return i;
+    }
+    function addition (int x, int y)public view returns(int){
+        return x+y;
+    }
+    function subtract (int x, int y)public view returns(int){
+        return x-y;
+    }
+    function multiplication (int x, int y)public view returns(int){
+        return x*y;
+    }
+    function division (int x, int y)public view returns(int){
+        return x/y;
+    }
+    function modulus (int x, int y)public view returns(int){
+        return x%y;
+    }
+}
